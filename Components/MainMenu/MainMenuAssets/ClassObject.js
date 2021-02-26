3f2c1a488c304eb4866f1c3ac903795724dfd656
therealstevew <winstanley.steven05@gmail.com>
2021-02-26 16:22:47 -0500

    Basic Menu Update Done

---
+++ Components/MainMenu/MainMenuAssets/ClassObject.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+const ClassObject = (props) => {
+  return (
+    <View style={styles.classIcon}>
+      <Image
+        style={styles.img}
+        source={{
+          uri:
+            'https://www.cnn.com/2010/POLITICS/06/08/rage.obama/t1largrage.gi.jpg',
+        }}
+      />
+      <Text style={styles.className}>Math or something lol</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  className: {
+    position: 'absolute',
+    width: wp('80%'),
+    color: '#fff',
+    fontFamily: 'monospace',
+    zIndex: 1,
+    padding: 20,
+    borderRadius: 10,
+    textAlign: 'left',
+  },
+  classIcon: {
+    flex: 1,
+    borderRadius: 10,
+    alignItems: 'center',
+    padding: 20,
+  },
+  img: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    position: 'relative',
+    width: wp('80%'),
+    height: 200,
+    borderRadius: 10,
+    elevation: 8,
+    zIndex: 1,
+  },
+});
+export default ClassObject;
--- Components/MainMenu/MainMenuScreen.js
+++ Components/MainMenu/MainMenuScreen.js
@@ -1,11 +1,18 @@
 import {SafeAreaView, Text, View} from 'react-native';
 import * as React from 'react';
+import Header from "./MainMenuAssets/Header";
+import ClassObject from "./MainMenuAssets/ClassObject";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 const MainMenuScreen = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{backgroundColor: 'lightgrey', height: hp('100%')}}>
       <View>
-        <Text>Main Menu</Text>
+        <Header/>
+        <ClassObject />
       </View>
     </SafeAreaView>
   );
