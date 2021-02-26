bac5fc1e0a2d84dcc2fdbde9a82d1bf18a0047f8
therealstevew <winstanley.steven05@gmail.com>
2021-02-26 16:03:09 -0500

    Just an update on UI branch dont mind me

---
+++ components /Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+const Header = ({title}) => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
+};
+Header.defaultProps = {
+  title: 'Study Buddy',
+};
+const styles = StyleSheet.create({
+  header: {
+    height: 60,
+    padding: 15,
+    backgroundColor: 'darkslateblue'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 23,
+    textAlign: 'left'
+  },
+});
+export default Header;
